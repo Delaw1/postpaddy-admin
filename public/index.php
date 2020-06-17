4905cf8c87306d95802de727dc40a70a0f94074e
iambowodavid <davidbowo@gmail.com>
2020-06-17 20:24:00 +0000

    .

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 define('LARAVEL_START', microtime(true));
