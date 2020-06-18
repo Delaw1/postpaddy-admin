6fb353a9a3e3655fcaecc6a924ed418b7ccc3fd0
iambowodavid <davidbowo@gmail.com>
2020-06-18 13:20:49 +0000

    .

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@
 // else{
 //     header("Access-Control-Allow-Origin: http://localhost:3000");
 // }
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
