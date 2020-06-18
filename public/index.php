ed35fc9ed16373e765716000a57531a10d9e4eb1
iambowodavid <davidbowo@gmail.com>
2020-06-18 06:56:19 +0000

    .

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@
  */
 
 header("Access-Control-Allow-Origin: http://localhost:3000");
+header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 define('LARAVEL_START', microtime(true));
