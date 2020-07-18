54f05e4fe0b134fd7368f84430c0d18b3a3ab299
delaw <lawrenceajayi481@gmail.com>
2020-07-18 09:35:03 +0100

    cors

--- app/User.php
+++ app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use Laravel\Passport\HasApiTokens;
+// use Laravel\Passport\HasApiTokens;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +11,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
-    use HasApiTokens;
+    // use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
