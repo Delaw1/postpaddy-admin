1a1362c3f54b277888d1db8536bb8c2f33662537
delaw <lawrenceajayi481@gmail.com>
2020-08-11 14:44:54 +0100

    Add facebook accoount

--- app/FacebookAccount.php
+++ app/FacebookAccount.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class FacebookAccount extends Model
 {
-    //
+    protected $fillable = [
+        'company_id', 'oauth_token', 'facebook_id', 'oauth_token_secret'
+    ];
+
+    protected $hidden = [
+        'linkedin_access_token',
+    ];
+
+    public function company(){
+        return $this->hasOne("App\Company", "id", "company_id");
+    }
 }
