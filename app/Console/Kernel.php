d6500f3c4cacb9a5ef6f219756c8d1ed2d52c7b2
Lawrence Ajayi <lawrenceajayi481@gmail.com>
2020-10-07 07:11:42 +0100

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         
-        $schedule->command('schedule:post')->everyFiveMinutes();
+        $schedule->command('schedule:post')->everyTwoMinutes();
     }
 
     /**
