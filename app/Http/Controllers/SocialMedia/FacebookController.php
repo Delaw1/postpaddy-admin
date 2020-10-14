4926c1c9a01563e1ebf232707a5cb91f525fd654
Lawrence Ajayi <lawrenceajayi481@gmail.com>
2020-10-14 23:35:04 +0100

    Update

--- app/Http/Controllers/SocialMedia/FacebookController.php
+++ app/Http/Controllers/SocialMedia/FacebookController.php
@@ -218,10 +218,10 @@ class FacebookController extends Controller
       } else {
         $photoIdArray = array();
         if (!empty($media) && $media != "[]") {
-          
+          $mmm = ["16027142263810.PNG"];
           // return response()->json($newMedia);
           $medi = strval($media[0]);
-          return $medi;
+          return $mmm[0];
           $url = "https://postslate.com/api/uploads/".$medi."";
           $photo = (Utils::curlPostRequest("https://graph.facebook.com/" . $account["id"] . "/photos", "url=" . $url . "&published=false&access_token=" . $account["access_token"], [], ["Content-Type: application/json"]));
           return response()->json($photo);
