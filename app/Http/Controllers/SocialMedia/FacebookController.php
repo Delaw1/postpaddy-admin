dcb01fc0f30ecdffb2f73cde7c1c4b14a1b76e58
Lawrence Ajayi <lawrenceajayi481@gmail.com>
2020-10-17 16:12:54 +0100

    Update

--- app/Http/Controllers/SocialMedia/FacebookController.php
+++ app/Http/Controllers/SocialMedia/FacebookController.php
@@ -27,8 +27,7 @@ class FacebookController extends Controller
     $this->fb = new Facebook([
       'app_id' => $clientID,
       'app_secret' => $clientSecret,
-      'default_graph_version' => 'v8.0',
-      //'default_access_token' => '{access-token}', // optional
+      'default_graph_version' => 'v3.0',
       'fileUpload' => true,
       'cookie' => true
     ]);
@@ -236,12 +235,12 @@ class FacebookController extends Controller
           try {
             // $data = ['url' => $url, 'published' => true];
             // $this->fb->setFileUploadSupport(true);
-            $source = '@'.public_path(Utils::UPLOADS_DIR . "/$media[0]");
-            $photo = (Utils::curlPostRequest("https://graph.facebook.com/" . $account["id"] . "/photos", "source=" . $source . "&published=false&access_token=" . $account["access_token"], [], ["Content-Type: application/json"]));
-            // $data = ['source' => '@'.public_path(Utils::UPLOADS_DIR . "/$media[0]"), 'published' => true];
+            $source = '@'.realpath(public_path(Utils::UPLOADS_DIR . "/$media[0]"));
+            // $photo = (Utils::curlPostRequest("https://graph.facebook.com/" . $account["id"] . "/photos", "source=" . $source . "&published=false&access_token=" . $account["access_token"], [], ["Content-Type: application/json"]));
+            $data = ['message' => $text, 'source' => $source];
             
-            // $response = $this->fb->post('/' . $account['id'] . '/photos', $data, $account['access_token']);
-            return response()->json($photo);
+            $response = $this->fb->post('/' . $account['id'] . '/photos', $data, $account['access_token']);
+            // return response()->json($photo);
           } catch (Facebook\Exceptions\FacebookResponseException $e) {
             return 'Graph returned an error: ' . $e->getMessage();
             exit;
