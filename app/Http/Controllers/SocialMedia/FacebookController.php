6d48cd11c671d716870a80e867b2a90d3e513296
Lawrence Ajayi <lawrenceajayi481@gmail.com>
2020-10-17 16:45:15 +0100

    Update

--- app/Http/Controllers/SocialMedia/FacebookController.php
+++ app/Http/Controllers/SocialMedia/FacebookController.php
@@ -237,9 +237,9 @@ class FacebookController extends Controller
             // $this->fb->setFileUploadSupport(true);
             // $source = '@'.realpath(public_path(Utils::UPLOADS_DIR . "/$media[0]"));
             // $this->fb->setFileUploadSupport(true);
-            $source = fopen('/var/www/api/public/uploads/postslate1602947680420.png', 'r');
+            $source = '/var/www/api/public/uploads/postslate1602947680420.png';
             // $photo = (Utils::curlPostRequest("https://graph.facebook.com/" . $account["id"] . "/photos", "source=" . $source . "&published=false&access_token=" . $account["access_token"], [], ["Content-Type: application/json"]));
-            $data = ['source' => $source];
+            $data = ['source' => $this->fb->fileToUpload($source)];
             
             $response = $this->fb->post('/' . $account['id'] . '/photos', $data, $account['access_token']);
             // return response()->json($photo);
