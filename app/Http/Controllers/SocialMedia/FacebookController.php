be30e0f04c005f180f15fb039b0f56377c52f66c
Lawrence Ajayi <lawrenceajayi481@gmail.com>
2020-10-17 12:54:55 +0100

    Update

--- app/Http/Controllers/SocialMedia/FacebookController.php
+++ app/Http/Controllers/SocialMedia/FacebookController.php
@@ -220,6 +220,7 @@ class FacebookController extends Controller
         if (!empty($media) && $media != "[]") {
           // $media[0] = "postslate1602934967234.png";
           $url = "https://www.postslate.com/api/uploads/" . $media[0];
+          return $url;
           // return redirect($url);
           // $mmm = ["16027142263810.PNG"];
           // // return response()->json([$media, $mmm]);
