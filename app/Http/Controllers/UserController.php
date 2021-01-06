70fa2da8c3a07cc17f02bd374aaa057f09a35e5f
Lawrence Ajayi <lawrenceajayi481@gmail.com>
2021-01-06 15:57:31 +0100

    Test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -290,8 +290,8 @@ class UserController extends Controller
 
         $response = Http::asForm()->post('https://www.postpaddy.com/api/oauth/token', [
             'grant_type' => 'password',
-                'client_id' => $oClient->id,
-                'client_secret' => $oClient->secret,
+                'client_id' => 4
+                'client_secret' => '$oClient->secre',
                 'username' => $request->input('email'),
                 'password' => $request->input('password'),
                 'scope' => '*'
